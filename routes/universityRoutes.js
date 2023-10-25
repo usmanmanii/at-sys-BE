7980abc4650a9e30c551dbaaabff27e7480a71bb
GitMuhammadAli <135626772+GitMuhammadAli@users.noreply.github.com>
2023-10-25 22:54:37 +0500

    updated

--- routes/universityRoutes.js
+++ routes/universityRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const universityController = require('../controllers/universityController')
-
+// ok
 
 router.post('/university', universityController.createUniversity);
 
