e68ae7677589f762d0ab9db98e1fb35652e9bc91
GitMuhammadAli <malingxoft@gmail.com>
2024-02-02 20:38:29 +0500

    models-changing

--- models/courseSchema.js
+++ models/courseSchema.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const courseSchema = new mongoose.Schema({
     Title: {type: String, required: true },
     Description: String,
-    Credits: Number,
-  Department: { type: mongoose.Schema.Types.ObjectId, ref: 'Department', required: true },
+  Credits: { type: Number , enum:[1,2,3], required: true},
+    Department: { type: mongoose.Schema.Types.ObjectId, ref: 'Department', required: true },
     delete: { type: Boolean, default: false }
     
   });
