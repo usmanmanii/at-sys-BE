ad072fc54a2b01f4b2145927a40156d29cda6f0c
M usman <m.usman@ideatolife.me>
2024-02-29 11:45:22 +0500

    some changes

--- authMiddleware.js
+++ authMiddleware.js
@@ -1,24 +1,39 @@
-// const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 
-// function authenticateToken(req, res, next) {
-//     // return res.send({
-//     //     body: JSON.stringify(req.body),
-//     //     params: JSON.stringify(req.params),
-//     //     headers: JSON.stringify(req.headers)
-//     // })
-//     console.log("MIddleware Paload Body => ", JSON.stringify(req.body));
-//     console.log("MIddleware Paload Params => ", JSON.stringify(req.params));
-//     console.log("MIddleware Paload Headers => ", JSON.stringify(req.headers));
-//   const token = req.headers.authorization;
-//   // const password =req.body.password;
-//   if (!token) return res.status(401).json({ message: "No token provided" });
-//   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-//     if (err)
-//       return res.status(403).json({ message: "Failed to authenticate token" });
-//     req.user = decoded;
-//     next();
-//   });
-// }
+function authenticateToken(req, res, next) {
+  // return res.send({
+  //     body: JSON.stringify(req.body),
+  //     params: JSON.stringify(req.params),
+  //     headers: JSON.stringify(req.headers)
+  // })
+  // console.log("MIddleware Paload Body => ", JSON.stringify(req.body));
+  // console.log("MIddleware Paload Params => ", JSON.stringify(req.params));
+  // console.log("MIddleware Paload Headers => ", JSON.stringify(req.headers));
+  if (req.headers && req.headers.authorization) {
+    const token = req.headers.authorization;
+    // const password =req.body.password;
+    if (!token) return res.status(401).json({ message: "No token provided" });
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) {
+        return res
+          .status(403)
+          .json({ message: "Failed to authenticate token", detail: err});
+      }
+      User.findOne({
+        _id: decoded._id,
+      }).exec((err, user) => {
+        if (err) {
+          res.status(500).send({
+            message: err,
+          });
+        } else {
+          req.user = user;
+          next();
+        }
+      });
+    });
+  }
+}
 
+module.exports = authenticateToken;
 
-// module.exports = authenticateToken;
